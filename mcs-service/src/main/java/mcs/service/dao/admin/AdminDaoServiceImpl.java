45977a8903e835de107e3f32b252240e8ae17de4
sahil.kapoor <sahil.kapoor@NDAMSERVPC101.markit.partners>
2014-09-15 18:30:30 +0530

    Database connection configurations added.

--- mcs-service/src/main/java/mcs/service/dao/admin/AdminDaoServiceImpl.java
+++ mcs-service/src/main/java/mcs/service/dao/admin/AdminDaoServiceImpl.java
@@ -1,5 +1,24 @@
 package mcs.service.dao.admin;
 
+import java.sql.Types;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class AdminDaoServiceImpl implements AdminDaoService{
 
+	
+	private JdbcTemplate jdbcTemplate;
+	 
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+	 this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	public int insertUser(String username, int id) {
+		 String inserQuery = "insert into test (id, name) values (?, ?) ";
+		 Object[] params = new Object[] { id,username };
+		 int[] types = new int[] { Types.INTEGER, Types.VARCHAR };
+		 return jdbcTemplate.update(inserQuery, params, types);
+		 
+		}
+	
 }
--- mcs-service/src/main/java/mcs/service/test/Test.java
+++ mcs-service/src/main/java/mcs/service/test/Test.java
@@ -1,19 +1,23 @@
 package mcs.service.test;
 
-import org.apache.jcs.JCS;
-import org.apache.jcs.access.CacheAccess;
-import org.apache.jcs.access.exception.CacheException;
+import mcs.service.dao.admin.AdminDaoServiceImpl;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
-	 private JCS bookCache;
-	
-	
-	public static void main(String[] args) throws Exception, CacheException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"database-mgmt.xml");
+	AdminDaoServiceImpl dao = applicationContext.getBean("adminDao", AdminDaoServiceImpl.class);
+	
+	dao.insertUser("sahil",1);
+	System.out.println("User inserted with id= ");	
+		/*
 		Test test=new Test();
 		test.bookCache=JCS.getInstance("default");
 	test.bookCache.put("1", "val1");
@@ -29,7 +33,7 @@ public class Test {
 	test.bookCache.putInGroup("4", "session", "req1");
 	test.bookCache.putInGroup("5", "session", "req1");
 	test.bookCache.getGroupKeys("session");
-	
+	*/
 	}
 
 }
