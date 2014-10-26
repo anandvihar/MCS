ed291cc56d1286e25775c855dc87c2818a177b8f
anandvihar <sahilkapoor1987@gmail.com>
2014-10-27 01:54:03 +0530

    Session Atrributes bean added

---
+++ mcs-ui-responsive/src/main/java/com/mcs/beans/SessionAttribute.java
@@ -0,0 +1,34 @@
+package com.mcs.beans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.SessionScoped;
+
+import mcs.rest.dao.pojo.User;
+
+@SessionScoped
+public class SessionAttribute implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public User user;
+	public String sessionId;
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getSessionId() {
+		return sessionId;
+	}
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	
+	
+	
+	
+}
